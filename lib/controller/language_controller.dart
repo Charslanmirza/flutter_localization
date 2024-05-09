548add9d5b0afe41c221f0692544e34d474c1a19
Arslan Mirza <arslan.qadeer@utorsols.com>
2024-05-09 12:58:48 +0500

    Initial commit

---
+++ lib/controller/language_controller.dart
@@ -0,0 +1,62 @@
+import 'dart:ui';
+
+import 'package:get/get.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_disposable.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../models/language_model.dart';
+import '../utils/app_constants.dart';
+
+class LocalizationController extends GetxController implements GetxService {
+  final SharedPreferences sharedPreferences;
+
+  LocalizationController({required this.sharedPreferences}) {
+    loadCurrentLanguage();
+  }
+
+  Locale _locale = Locale(AppConstants.languages[0].languageCode,
+      AppConstants.languages[0].countryCode);
+
+  int _selectedIndex = 0;
+  int get selectedIndex => _selectedIndex;
+  List<LanguageModel> _languages = [];
+  Locale get locale => _locale;
+  List<LanguageModel> get languages => _languages;
+
+  void loadCurrentLanguage() {
+    // only gets called during installation or rebooting
+    _locale = Locale(
+        sharedPreferences.getString(AppConstants.LANGUAGE_CODE) ??
+            AppConstants.languages[0].languageCode,
+        sharedPreferences.getString(AppConstants.COUNTRY_CODE) ??
+            AppConstants.languages[0].countryCode);
+
+    for (int index = 0; index < AppConstants.languages.length; index++) {
+      if (AppConstants.languages[index].languageCode == _locale.languageCode) {
+        _selectedIndex = index;
+        break;
+      }
+    }
+    _languages = [];
+    _languages.addAll(AppConstants.languages as Iterable<LanguageModel>);
+    update();
+  }
+
+  void setLanguage(Locale locale) {
+    Get.updateLocale(locale);
+    _locale = locale;
+    saveLanguage(_locale);
+    update();
+  }
+
+  void setSelectedIndex(int index) {
+    _selectedIndex = index;
+    update();
+  }
+
+  void saveLanguage(Locale locale) async {
+    sharedPreferences.setString(
+        AppConstants.LANGUAGE_CODE, locale.languageCode);
+    sharedPreferences.setString(AppConstants.COUNTRY_CODE, locale.countryCode!);
+  }
+}
---
+++ lib/utils/language_widget.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import '../controller/language_controller.dart';
+import '../models/language_model.dart';
+import 'app_constants.dart';
+
+class LanguageWidget extends StatelessWidget {
+  final LanguageModel languageModel;
+  final LocalizationController localizationController;
+  final int index;
+  LanguageWidget(
+      {required this.languageModel,
+      required this.localizationController,
+      required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+        onTap: () {
+          localizationController.setLanguage(Locale(
+            AppConstants.languages[index].languageCode,
+            AppConstants.languages[index].countryCode,
+          ));
+          localizationController.setSelectedIndex(index);
+        },
+        child: Container(
+          padding: EdgeInsets.all(7),
+          child: Center(
+            child: Column(
+              // mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+                  children: [
+                    Text(languageModel.imageUrl,
+                        style: TextStyle(fontSize: 20)),
+                    SizedBox(
+                      width: 8,
+                    ),
+                    Text(
+                      languageModel.languageName,
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: localizationController.selectedIndex == index
+                              ? Colors.blue
+                              : Colors.black),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+}
